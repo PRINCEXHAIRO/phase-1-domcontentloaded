7cf18292f42c791445da27121345d6fac5332448
Hairo Barnard <Hairo.barnard@gmail.com>
2023-11-05 11:51:13 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const text = document.getElementById('text')
+
+console.log(text)
+
+function sayThis() { 
+    document.getElementById("text").textContent = "This is really cool!"
+}
+
+document.addEventListener('DOMContentLoaded', sayThis)
+
+console.log(sayThis)
